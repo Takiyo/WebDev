cd0ed56394bc138afe0b7df945d9d78e64c9093a
Takiyo <takiyobrytowski@gmail.com>
2018-09-29 22:57:56 -0500

    start madlibs

---
+++ projects/project01/MadLibs.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+    <legend>Enter a noun:</legend>
+    <input type="text" name="noun"/><br><br>
+    <legend>Enter a verb:</legend>
+    <input type="text" name="verb"/><br><br>
+    <legend>Enter an adjective:</legend>
+    <input type="text" name="adjective"/><br><br>
+    <legend>Enter an adverb:</legend>
+    <input type="text" name="adverb"/><br><br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</form>
+
