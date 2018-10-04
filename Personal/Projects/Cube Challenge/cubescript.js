0eb4d1c565078d6f0b5b2637d7b36df2ac3c2f6c
wurk <15766412@mstc.edu>
2018-10-03 19:49:17 -0500

    fuggin light n shit

--- Personal/Projects/Cube Challenge/cubescript.js
+++ Personal/Projects/Cube Challenge/cubescript.js
@@ -7,6 +7,9 @@ let w = 20;
 let ma;
 let maxD;
 
+let xlightcounter = 0;
+let ylightcounter = 0;
+
 function setup(){
   createCanvas(400, 400, WEBGL);
   ma = atan(sqrt(1/sqrt(2)));
@@ -14,27 +17,30 @@ function setup(){
 }
 
 function draw(){
-  background(100);
+  background(20);
   ortho(-300, 300, 300, -300, -500, 700);
 
-  directionalLight(255, 255, 255, 0, -1, 0);
+  directionalLight(100, 0, 255, 2, -1, 0);
+  directionalLight(255, 255, 0, map(mouseX, 0, width, 0, 5), -1, 0);
+
+
 
 
   rotateX(QUARTER_PI);
   rotateY(ma);
 
 
-
-
 for (let z = 0; z < height; z += w){
   for (let x = 0; x < width; x += w){
     push();
+
+
     let d = dist(x,z,width/2,height/2);
     let offset = map(d,0,maxD, -2, 2);
     let a = angle + offset;
     let h = map(sin(a), -1, 1, 0, 200);
     translate(x - width / 2, 0, z - height / 2);
-    normalMaterial();
+    ambientMaterial(255, 255, 255);
     box(w-2, h, w); // width, height, depth
     //rect((x - width / 2) + (w / 2), 0, w - 2.5, h); // (x, y, width, height);
     pop();
