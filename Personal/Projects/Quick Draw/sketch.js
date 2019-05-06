50c0784c86dc39cec083955970afdaddcf1dc2fa
Takiyo <takiyobrytowski@gmail.com>
2019-05-06 14:08:20 -0500

    comments, cleaning, dimensions, and button testing

--- Personal/Projects/Quick Draw/sketch.js
+++ Personal/Projects/Quick Draw/sketch.js
@@ -1,23 +1,31 @@
+//TODOs
+//implement buttons for different modes like flying, editing, flat?, etc.
+//check if less possible RGB values makes it more efficient
+
+
 var cols, rows;
 var scl = 20;
-var w = 800;
-var h = 1000;
+var w = 600;
+var h = 600;
 var terrain;
 var flying = 0;
-var test1;
-var test2;
+var testButton;
+var test;
 
 function setup() {
   createCanvas(w,h,WEBGL);
-  cols = w / scl*1.8;
+  cols = w / scl*1.5;
   rows = h / scl;
+  button = createButton('click me');
+  button.position(19, 19);
+  button.mousePressed(changeBG);
   //frameRate(60);
-  textSize(30);
+  //textSize(30);
 
   //determine array size
   let arraySize = Math.round(max(rows,cols)) + 1;
 
-  //pre-fill array with dummy data
+  //create and pre-fill array
   terrain = [...Array(arraySize)].map(e => Array(arraySize).fill(1));
 }
 
@@ -38,7 +46,6 @@ function draw() {
     let xoff = 0;
     for (let x = 0; x < cols; x++){
       terrain[x][y] = map(noise(xoff, yoff), 0, 1, valley, hill);
-      //test1 = terrain[x][y];
       xoff += 0.1;
     }
     yoff += 0.1;
@@ -47,13 +54,33 @@ function draw() {
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
       beginShape();
+
+    //  testval2 = map(terrain[x][y], valley, hill, 0, 255);
+      //if (testval2 < 64){
+    //    stroke(0);
+    //  } else if (testval2 < 128){
+    //    stroke(64);
+    //  } else if (testval2 < 192){
+    //    stroke(128);
+    //  } else {
+    //    stroke(192);
+    //  }
+
       stroke(map(terrain[x][y], valley, hill, 0, 255));
       vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
       vertex((x+1)*scl, (y+1)*scl, terrain[x+1][y+1]);
       vertex((x+1)*scl, y*scl, terrain[x+1][y]);
+
+      //using (CLOSE) instead of drawing last connecting vertex
+      //not sure if more/less efficient
       //vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
       endShape(CLOSE);
 
     }
   }
+}
+
+function changeBG() {
+  var val = random(255);
+  background(val);
 }
