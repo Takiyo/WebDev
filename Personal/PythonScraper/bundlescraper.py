0a7c24ea12afce6d2563181ea3898cff302b9b9a
Takiyo <takiyobrytowski@gmail.com>
2019-08-09 15:58:10 -0500

    23:23 possible problem

--- Personal/PythonScraper/bundlescraper.py
+++ Personal/PythonScraper/bundlescraper.py
@@ -2,5 +2,11 @@
 #http://2.python-requests.org/en/master/
 
 import requests
+from bs4 import BeautifulSoup
 url = "https://www.humblebundle.com/books/cloud-computing-books"
-r = requests.get(url)
+resp = requests.get(url)
+
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+# Bundle Tiers
+# twos
