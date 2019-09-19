869a98160fecf83276b521bd91c2068b320118da
Takiyo <takiyobrytowski@gmail.com>
2019-09-19 15:58:04 -0500

    yeyyileieaeiofj

---
+++ InClass/objects/ExistingTables.php
@@ -0,0 +1,97 @@
+<!--
+
+Everything below the createPage function is part of the function.
+    This is so you can modularly create the same page on another file
+    but link different style sheets/scripts/etc
+
+    TODO - 
+        PDO
+        Delete topPlayersUse
+-->
+
+
+<?php
+$page = new ExistingTables();
+$page->createPage();
+class ExistingTables{
+    public function __construct(){
+
+    }
+
+    public function createPage(){
+        ?>
+
+    <h2>DBFZ Existing Tables</h2>
+
+
+    <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method="post">
+        <?php
+
+        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if ($dbc->connect_error){
+        die("Connection error: " . $dbc->connect_error);
+        }
+
+        if (isset($_POST['displayBTN']) && isset($_POST['checked_values'])) {
+            echo '<table style="width:100%" border="1px solid;">
+            <tr>
+                <th>playerId</th>
+                <th>playerName</th>
+                <th>characterName</th>
+                <th>datePlayerStarted</th>
+                <th>coolnessRating</th>      
+            </tr>';
+
+            // Displays Selected Values
+            foreach ($_POST['checked_values'] as $checked_id)
+            {
+
+                $query = "SELECT playerId, playerName, characterName, datePlayerStarted, coolnessRating
+            FROM dbfzcharacter WHERE playerId = $checked_id";
+
+                $result = mysqli_query($dbc, $query)
+                    or die ('Error querying database');
+
+                $row = mysqli_fetch_array($result);
+
+                //if ($row['topPlayersUse'] == '0'){
+                //    $row['topPlayersUse'] = "True";
+                //} else $row['topPlayersUse'] = "False";
+
+                    echo '<tr><td>' . $row['playerId'] . '</td><td>'
+                        . $row['playerName'] . '</td><td>'
+                        . $row['characterName'] . '</td><td>'
+                        . $row['datePlayerStarted'] . '</td><td>'
+                        . $row['coolnessRating'] . '</td></tr>';
+                }
+                echo '</table>';
+            }
+
+            // Deletes Selected Values
+        foreach ($_POST['checked_values'] as $checked_id) {
+
+            if (isset($_POST['deleteBTN']) && isset($_POST['checked_values'])) {
+                $query = "DELETE FROM dbfzcharacter WHERE playerId = $checked_id";
+                mysqli_query($dbc, $query);
+                echo '<h4 class="error">Record ' . $checked_id . ' deleted.</h4>';
+            }
+        }
+
+        $query = 'SELECT playerId FROM dbfzcharacter';
+        $result = mysqli_query($dbc, $query)
+            or die('Error querying database');
+
+            while ($row = mysqli_fetch_array($result))
+            {
+                echo '<input type="checkbox" value="' . $row['playerId']
+                    . '" name="checked_values[]">' . $row['playerId'] . "</input><br>";
+            }
+        mysqli_close($dbc);
+            ?>
+
+        <input name="displayBTN" type="submit" value="Display">
+        <input name="deleteBTN" type="submit" value="Delete">
+    </form>
+            <?php
+        }
+}
