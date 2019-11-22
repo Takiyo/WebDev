16596b243751e2ca1dc8cca4f1a03d756c71df62
Takiyo <takiyobrytowski@gmail.com>
2019-11-22 11:46:22 -0600

    Work on appearance

--- Tutorial/application/views/news/index.php
+++ Tutorial/application/views/news/index.php
@@ -9,3 +9,17 @@
         <p><a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View article</a></p>
 
 <?php endforeach; ?>
+
+
+<!-- placeholder -->
+<div class="position-absolute w-100 h-100" style="background-color: rgba(255,0,0,0.1);">
+    <div class="row align-items-center h-100 m-3">
+        <div class="col-sm"> 
+        </div>
+        <div class="col-sm card card-block mx-auto h-50">
+            card
+        </div> <!-- col -->
+        <div class="col-sm"> 
+        </div>
+    </div> <!-- row -->
+</div> container
--- drives/week11/application/views/forms/create.php
+++ drives/week11/application/views/forms/create.php
@@ -1,18 +1,12 @@
-<h2>form</h2>
 
-<?php echo validation_errors(); ?>
-
-<?php echo form_open('forms/create'); ?>
-
-    <label for="test2">Test2</label>
-    <input type="text" name="test2" /><br />
-
-    <label for="test3">Test3</label>
-    <textarea name="test3"></textarea><br />
-
-    <label for="test4">Test4</label>
-    <textarea name="test4"></textarea><br />
-
-    <input type="submit" name="submit" value="Create Product" />
-
-</form>
+<div class="position-absolute w-100 h-100" style="background-color: rgba(255,0,0,0.1);">
+    <div class="row align-items-center h-100 m-3">
+        <div class="col-sm"> 
+        </div>
+        <div class="col-sm card card-block mx-auto h-50">
+            card
+        </div> <!-- col -->
+        <div class="col-sm"> 
+        </div>
+    </div> <!-- row -->
+</div> <!-- container -->
