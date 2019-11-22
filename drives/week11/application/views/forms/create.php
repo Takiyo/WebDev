f5ae5052b97015c74d1220593e3533b6714e8c5d
Takiyo <takiyobrytowski@gmail.com>
2019-11-22 14:58:59 -0600

    dasdasd

--- drives/week11/application/views/forms/create.php
+++ drives/week11/application/views/forms/create.php
@@ -4,7 +4,23 @@
         <div class="col-sm"> 
         </div>
         <div class="col-sm card card-block mx-auto h-50">
-            card
+            <div class="mx-auto my-auto">
+                <?php echo validation_errors(); ?>
+
+                <?php echo form_open('forms/create'); ?>
+
+                    <label for="test2">Test2</label>
+                    <input type="text" name="test2" /><br />
+
+                    <label for="test3">Test3</label>
+                    <textarea name="test3"></textarea><br />
+
+                    <label for="test4">Test4</label>
+                    <textarea name="test4"></textarea><br />
+
+                    <input type="submit" name="submit" value="Create Product" />
+                </form>
+            </div> <!-- centered div -->
         </div> <!-- col -->
         <div class="col-sm"> 
         </div>
